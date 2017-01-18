4c1fc2f6b01a27f14b575f204470516539f4d62b
Romain <assie.romain@gmail.com>
2017-01-18 14:23:14 +0100

    Add content div

--- src/pages/show_off.rs
+++ src/pages/show_off.rs
@@ -10,10 +10,11 @@ impl ShowOff {
         html!{
             link rel="stylesheet" type="text/css"
                 href=("http://".to_string()+host+"/style/style.css") /
-
-                @for image in &data.data {
-                    a href=("http://".to_string()+host+"/"+&image.id.to_string()){
-                        img.photoGallery src=("http://".to_string()+host+"/images/min"+&image.src.split_at(9).1){}
+                div.gallery {
+                    @for image in &data.data {
+                        a href=("http://".to_string()+host+"/"+&image.id.to_string()){
+                            img.photoGallery src=("http://".to_string()+host+"/images/min"+&image.src.split_at(9).1){}
+                        }
                     }
                 }
         }
