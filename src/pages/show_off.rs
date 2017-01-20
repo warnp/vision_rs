e2b917e5ee8acc7ad77971ecde054cca2c9062d2
romain <romain.assie@supinfo.com>
2017-01-20 19:13:12 +0100

    Something

--- src/pages/show_off.rs
+++ src/pages/show_off.rs
@@ -11,9 +11,16 @@ impl ShowOff {
             link rel="stylesheet" type="text/css"
                 href=("http://".to_string()+host+"/style/style.css") /
 
-                @for image in &data.data {
-                    a href=("http://".to_string()+host+"/"+&image.id.to_string()){
-                        img.photoGallery src=("http://".to_string()+host+"/images/min"+&image.src.split_at(9).1){}
+                @let mut day = &data.data[0].jour {
+                    @for image in &data.data {
+                        @if day != &image.jour {
+                            h1 "Jour"
+                            h1 (&image.jour)
+                            day = &image.jour {}
+                        }
+                        a href=("http://".to_string()+host+"/"+&image.id.to_string()){
+                            img.photoGallery src=("http://".to_string()+host+"/images/min"+&image.src.split_at(9).1){}
+                        }
                     }
                 }
         }
